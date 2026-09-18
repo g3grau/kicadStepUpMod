4af4411024fd80934cf5f3d3aedb6ca281707c7b
Guenter Grau <mail@guenter-grau.de>
2026-09-18 22:28:40 +0200

    Bump cached hole fix marker

--- tracks.py
+++ tracks.py
@@ -3,7 +3,7 @@
 #****************************************************************************
 
 global tracks_version
-tracks_version = '2.7.1-k10fix6.20260918'
+tracks_version = '2.7.1-k10fix7.20260918'
 
 import kicad_parser
 #import kicad_parser; import importlib; importlib.reload(kicad_parser)
